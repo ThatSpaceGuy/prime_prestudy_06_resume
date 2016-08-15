05f8f6990e39dae68484b4a5cf4497b8ddd2ecbf
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-14 22:36:21 -0500

    Changing hover behavior

--- application.js
+++ application.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
   $('button').on('click',function(){
     $('.resume').toggle();
-    $('button').toggleClass('btn-success');
-    $('button').toggleClass('btn-danger');
+    $('button').toggleClass('btn-warning');
+    $('button').toggleClass('btn-info');
 
     if ($('.changable').text() == 'Please show') {
       $('.changable').text('Please hide');
     } else {
       $('.changable').text('Please show');
     }
+  });
+
+  $('button').on('mouseenter mouseleave',function(){
+
+    if ($('.changable').text() == 'Please show') {
+      $('button').toggleClass('btn-success');
+      $('button').toggleClass('btn-info');
+    } else {
+      $('button').toggleClass('btn-danger');
+      $('button').toggleClass('btn-warning');
+    }
+
   });
 });
